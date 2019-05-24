d18351c9722376c7a13e3b83a7570c5545fe0876
Shou Ya <shouya@users.noreply.github.com>
2019-05-25 04:21:39 +0800

    remove useless tags for sentry

--- apps/extension/config/config.exs
+++ apps/extension/config/config.exs
@@ -38,7 +38,5 @@ config :sentry,
   environment_name: Mix.env(),
   included_environments: [:prod, :dev],
   enable_source_code_context: true,
-  root_source_code_path: File.cwd!(),
-  tags: %{
-    env: "production"
-  }
+  root_source_code_path: File.cwd!()
+
